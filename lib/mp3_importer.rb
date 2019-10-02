02e21e98da30014c19b862e92a03c1cac5450603
Adam Weissman <adam@comedytragedyepic.com>
2019-10-02 08:56:09 +0000

    Automatically backed up by Learn

--- lib/mp3_importer.rb
+++ lib/mp3_importer.rb
@@ -7,7 +7,7 @@ class MP3Importer
   end
 
   def files
-    @files ||= Dir.glob("#{huh}/*.mp3").collect{ |f| f.gsub("#{huh}/", "") }
+    @files ||= Dir.glob("#{@path}/*.mp3").collect{ |f| f.gsub("#{@path}/", "") }
   end
 
   def import
